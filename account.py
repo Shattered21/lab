e8d1156767f9edf82573d49029262d04d6023c87
Shattered <113565672+Shattered21@users.noreply.github.com>
2022-11-09 13:12:38 -0600

    documented code

--- account.py
+++ account.py
@@ -1,33 +1,49 @@
+
+
 class Account:
 
+    def __init__(self, name: str) -> None:
 
-    def __init__(self, name):
+        self.__account_name = name  # Name of the account
+        self.__account_balance = 0.00  # Balance of the account
 
-        self.__account_name = name
-        self.__account_balance = 0
-
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Method that increase account balance
+        :param amount: amount the user wants to deposit into account
+        """
         if amount <= 0:
             return False
         else:
             self.__account_balance + amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        Method that reduces the amount of account balance
+        :param amount: amount user wants to take out of account balance
+        '''
         if (amount <= 0) or (amount < self.__account_balance):
             return False
         else:
             self.__account_balance - amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        Method to get account balance
+        :returns: account balance
+        '''
         return self.__account_balance
 
-    def get_name(self):
-        return self.__account_name
 
+    def get_name(self) -> str:
+        '''
+        Method to get account name
+        :returns: account name
+        '''
+        return self.__account_name  # Returns the name of the account
 
-if __name__ == '__main__':
 
 
 
